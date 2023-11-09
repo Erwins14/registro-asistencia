80969f5086c226cb50ad1b8a07e73d4a99d8bca7
Erwins14 <erwins1414@gmail.com>
2023-11-09 05:19:45 -0600

    Creación de Componentes

--- registro-asistencia/src/app/app-routing.module.ts
+++ registro-asistencia/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListUsersComponent } from './components/list-users/list-users.component';
+import { CreateUserComponent } from './components/create-user/create-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'list-users', pathMatch: 'full' },
+  { path: 'list-users', component: ListUsersComponent },
+  { path: 'create-user', component: CreateUserComponent },
+  { path: '**', redirectTo: 'list-users', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- registro-asistencia/src/app/app.module.ts
+++ registro-asistencia/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { ListUsersComponent } from './components/list-users/list-users.component';
 import { CreateUserComponent } from './components/create-user/create-user.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"esp8266-ceb36","appId":"1:258593487835:web:9d1c948712871d4d3545fe","databaseURL":"https://esp8266-ceb36-default-rtdb.firebaseio.com","storageBucket":"esp8266-ceb36.appspot.com","apiKey":"AIzaSyAaApo7cviHp2gG-7YnhNCCz9vaUThu1x4","authDomain":"esp8266-ceb36.firebaseapp.com","messagingSenderId":"258593487835"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
