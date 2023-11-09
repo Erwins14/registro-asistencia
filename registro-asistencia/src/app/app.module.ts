99419873ad194d9498ccc396fb9d843ca8f9694f
Erwins14 <erwins1414@gmail.com>
2023-11-09 01:23:46 -0600

    Creación componentes

--- registro-asistencia/src/app/app.module.ts
+++ registro-asistencia/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListUsersComponent } from './components/list-users/list-users.component';
+import { CreateUserComponent } from './components/create-user/create-user.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListUsersComponent,
+    CreateUserComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
