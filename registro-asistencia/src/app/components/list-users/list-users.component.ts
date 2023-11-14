287ecc8effb4839f6b68d4480b07d805e7653baa
Erwins14 <erwins1414@gmail.com>
2023-11-14 03:59:07 -0600

    Listado de Empleados

--- registro-asistencia/src/app/components/list-users/list-users.component.ts
+++ registro-asistencia/src/app/components/list-users/list-users.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Database, onValue, ref } from '@angular/fire/database';
+
 
 @Component({
   selector: 'app-list-users',
   templateUrl: './list-users.component.html',
   styleUrls: ['./list-users.component.css']
 })
+
 export class ListUsersComponent implements OnInit{
   
-  constructor() {}
+  usuarios: Array<any>=[]
 
-  ngOnInit(): void {}
+  constructor(public database:Database) {}
+
+  ngOnInit(): void {
+    this.obtenerUsuario();
+  }
+
+  obtenerUsuario(){
+    const starCountRef = ref(this.database, 'usuarios/');
+    onValue(starCountRef, (snapshot) => {
+      snapshot.forEach((childSnapshot) => {
+        const childData = childSnapshot.val();
+        let usuario = {cardUID:childData.cardUID,nombre:childData.nombre,apellido:childData.apellido,userStatus:childData.userStatus};
+        this.usuarios.push(usuario);
+      });
+    });
+  }
 
 }
